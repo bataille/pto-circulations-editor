c8451c4d0ae948cd95e034c4dfed8cebb648a531
Benoit Bataille <benoit.bataille@gmail.com>
2020-06-29 01:29:55 +0200

    Updates

--- precache-manifest.0e6f3c5997dff9b672793619d12300c8.js
+++ precache-manifest.0e6f3c5997dff9b672793619d12300c8.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "68fd25b7b33f2f4814c8512bea9727c6",
+    "revision": "e59f2049adc653b75441c8af49fc7416",
     "url": "/pto-circulations-editor/index.html"
   },
   {
-    "revision": "289bc5bb1a35527fae82",
+    "revision": "53bf87a82e33c3fa5427",
     "url": "/pto-circulations-editor/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "cff9992c2a898857ee2a",
+    "revision": "03d63c7d0159e423fc01",
     "url": "/pto-circulations-editor/static/css/main.780a517e.chunk.css"
   },
   {
-    "revision": "289bc5bb1a35527fae82",
-    "url": "/pto-circulations-editor/static/js/2.bbae6ee3.chunk.js"
+    "revision": "53bf87a82e33c3fa5427",
+    "url": "/pto-circulations-editor/static/js/2.941458cc.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/pto-circulations-editor/static/js/2.bbae6ee3.chunk.js.LICENSE.txt"
+    "url": "/pto-circulations-editor/static/js/2.941458cc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "cff9992c2a898857ee2a",
-    "url": "/pto-circulations-editor/static/js/main.7b690031.chunk.js"
+    "revision": "03d63c7d0159e423fc01",
+    "url": "/pto-circulations-editor/static/js/main.582a7644.chunk.js"
   },
   {
     "revision": "2fe9270066692a97438a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pto-circulations-editor/precache-manifest.8ab77b0e72a27337b3aa17eab4d1cdc9.js"
+  "/pto-circulations-editor/precache-manifest.0e6f3c5997dff9b672793619d12300c8.js"
 );
 
 self.addEventListener('message', (event) => {
