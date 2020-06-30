6c3322ab4bfb4d03e560f306ada48cbc14396619
Benoit Bataille <benoit.bataille@gmail.com>
2020-06-30 16:02:05 +0200

    Updates

---
+++ precache-manifest.2a19154725e08039aa23040292be239f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af7612db43fb941ec799f20403d9330c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "53bf87a82e33c3fa5427",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "83284f39b77005de49b2",
+    "url": "./static/css/main.780a517e.chunk.css"
+  },
+  {
+    "revision": "53bf87a82e33c3fa5427",
+    "url": "./static/js/2.941458cc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.941458cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83284f39b77005de49b2",
+    "url": "./static/js/main.dd0f716b.chunk.js"
+  },
+  {
+    "revision": "cc5494bd602a134554b4",
+    "url": "./static/js/runtime-main.7a0469e7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pto-circulations-editor/precache-manifest.0e002349163386c08a2b18b9d0dc9878.js"
+  "./precache-manifest.2a19154725e08039aa23040292be239f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pto-circulations-editor/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
